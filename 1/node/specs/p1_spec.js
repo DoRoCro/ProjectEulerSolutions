636f1c909b4c748a7ac237328dd15b671958b26f
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-13 20:26:55 +0100

    add some test for completeness

--- 1/node/specs/p1_spec.js
+++ 1/node/specs/p1_spec.js
@@ -7,7 +7,13 @@ describe('P1', function () {
     // p1 = new p1()
   })
   it('should calculate 23 for 10', function () {
-    assert.strictEqual(23, p1(10))
+    assert.strictEqual( p1(10), 23)
+  })
+  it('should calculate 3 for 4', function () {
+    assert.strictEqual(p1(4),3)
+  })
+  it('should calculate 233168 for 1000', function () {
+    assert.strictEqual( p1(1000), 233168)
   })
 
 })
