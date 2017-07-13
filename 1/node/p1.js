e8627a76cc3225d6918dff43be7f57d32af2d3d5
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-13 19:05:11 +0100

    P1 node now calculates an answer

--- 1/node/p1.js
+++ 1/node/p1.js
@@ -1,5 +1,10 @@
 var p1 = function (number) {
-  var result = 23
+  var result = 0
+  for( var i=1;i<number; i++) {
+    if (i % 3 === 0 || i % 5 === 0 ){
+      result += i
+    }
+  }
   return result
 } 
 
