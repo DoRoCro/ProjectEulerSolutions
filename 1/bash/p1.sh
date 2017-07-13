5c19c4a07d35da0b2f5d90d5d5c545f444c63f87
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-13 16:49:12 +0100

    spreadsheet and bash solutions

---
+++ 1/bash/p1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# I'm sure csh used to be easier than this
+sum=0
+for i in `seq 1 999`
+do
+    if [[ ( $[ $i % 3 ] == 0 ) || ( $[ $i % 5 ] == 0 ) ]]; then
+        sum=$(($sum+$i))
+    fi
+done
+echo $sum
