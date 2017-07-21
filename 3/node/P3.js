052a7aea2398850f2a7d0fc367d1d7c5974516dd
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-21 18:26:12 +0100

    P3 JavaScript solution with tests

---
+++ 3/node/P3.js
@@ -0,0 +1,37 @@
+var p3 = function (number) {
+  // 'borrowed' from http://www.javascripter.net/math/primes/factorization.htm
+  if (isNaN(number) || !isFinite(number) || number % 1 !== 0 || number === 0) return number
+  if (number < 0) return '-' + p3(-number)
+  var minFactors = []
+  if (number > 1) {
+    minFactors = minFactors.concat(leastFactor(number))
+  }
+  console.log('called')
+  console.log(minFactors)
+  return minFactors.concat(p3(number / minFactors[minFactors.length - 1]))
+}
+
+// find the least factor in n by trial division
+function leastFactor (n) {
+  if (isNaN(n) || !isFinite(n)) return NaN
+  if (n === 0) return 0
+  if (n % 1 || n * n < 2) return 1
+  if (n % 2 === 0) return 2
+  if (n % 3 === 0) return 3
+  if (n % 5 === 0) return 5
+  var m = Math.sqrt(n)
+  for (var i = 7; i <= m; i += 30) {
+    if (n % i === 0) return i
+    if (n % (i + 4) === 0) return i + 4
+    if (n % (i + 6) === 0) return i + 6
+    if (n % (i + 10) === 0) return i + 10
+    if (n % (i + 12) === 0) return i + 12
+    if (n % (i + 16) === 0) return i + 16
+    if (n % (i + 22) === 0) return i + 22
+    if (n % (i + 24) === 0) return i + 24
+  }
+  return n
+}
+// const answer = p3(13195)
+const answer = p3(600851475143)
+console.log('the answer is: ' + answer[answer.length - 2])
---
+++ 3/node/specs/P3_spec.js
@@ -0,0 +1,15 @@
+var assert = require('assert')
+var p3 = require('../P3.js')
+describe('p3', function () {
+  // var p1
+
+  beforeEach('before each', function () {
+    // p1 = new p1()
+  })
+  it('should calculate 29 for 13195', function () {
+    assert.strictEqual( p3(13195), 29)
+  })
+  it('should calculate 5 for 15', function () {
+    assert.strictEqual(p3(15), 5)
+  })
+})
