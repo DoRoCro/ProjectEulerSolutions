6b780d8b0789296a5fba84398e21f3dd193ba875
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-21 16:05:08 +0100

    now use built in Prime class to show achievable in reasonable time

--- 3/ruby/P3.rb
+++ 3/ruby/P3.rb
@@ -1,21 +1,9 @@
 require 'prime'
 class MyPrime
-
   def self.largest_factor number
-    # stop if prime (feels like cheating...)
-    return number if Prime::prime? number
-
-    # find all potential factors up to number/2
-    primes = []
-    Prime.each(number/2) do |prime|
-      primes.push(prime)
-    end
-    # filter out all non-factors
-    primes.reject! {|prime| number % prime != 0}
-    # return last item remaining in array
-    return primes.last
+    return Prime.prime_division(number).last[0]
   end
 end
-answer = MyPrime.largest_factor 600851475143
 # answer = MyPrime.largest_factor 13195
+answer = MyPrime.largest_factor 600851475143
 puts "The answer is: #{answer}"
