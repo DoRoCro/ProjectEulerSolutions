fc90cdff46c88ab490c6ff7700f8f88ec6081f49
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-07-28 20:40:27 +0100

    update using Prime functions to streamline

--- 5/ruby/P5.rb
+++ 5/ruby/P5.rb
@@ -1,24 +1,35 @@
+require 'prime'
 class P5
   attr_reader :number
 
   def initialize(number)
     @number = number
   end
-  
+
   def divisible?(num)
     (1..@number).to_a.each do |divisor|
       return false if num % divisor != 0
     end
     return true
   end
-  
+
   def find_min_multiple
-    multiple = @number * 2
+    # try to assemble min multiple from prime factors
     found = false
-    until found do 
-      multiple += 2
-      found = true if self.divisible?(multiple) 
+    primes = {2 => 1}
+    for i in 2..@number do
+      factors = i.prime_division
+      puts "primes = #{primes}"
+      puts "i= #{i} factors= #{factors}"
+
+      factors.each do |fac|
+        puts "fac[0]= #{fac[0]}"
+        primes[fac[0]] = primes[fac[0]] ? [primes[fac[0]], fac[1]].max : fac[1]
+      end
     end
+    multiple = 1
+    puts primes
+    primes.each { |p,e| multiple *= p ** e }
     return multiple
   end
 end
